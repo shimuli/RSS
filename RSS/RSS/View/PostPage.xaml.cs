fd4ee3bd9e028551976bced5a18bb4479b9e1cd4
shimuli <shimulicedric@gmail.com>
2022-01-17 07:59:48 +0300

    Add project files.

---
+++ RSS/RSS/View/PostPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using RSS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RSS.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PostPage : ContentPage
+    {
+        public PostPage()
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+        }
+
+        public PostPage(Item item)
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+
+            webView.Source = item.ItemLink;
+        }
+    }
+}
---
+++ RSS/RSS/ViewModel/MainVM.cs
@@ -0,0 +1,46 @@
+﻿using RSS.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace RSS.ViewModel
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public Posts Blog
+        {
+            get;
+            set;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainVM()
+        {
+            ReadRss();
+        }
+
+        public void ReadRss()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Posts));
+
+            using (WebClient client = new WebClient())
+            {
+                string xml = Encoding.Default.GetString(client.DownloadData("https://cointelegraph.com/rss"));
+                using (Stream reader = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
+                {
+                    Blog = (Posts)serializer.Deserialize(reader);
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
